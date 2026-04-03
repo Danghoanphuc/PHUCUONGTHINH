a4b170a5c38d863364dbd7a242dda25ccfa23c88
Printz Viet Nam Company <phucdh911@gmail.com>
2026-04-03 10:17:51 +0700

    fix loi upload

--- packages/backend/src/documents/documents.module.ts
+++ packages/backend/src/documents/documents.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { MediaModule } from '../media/media.module';
 
 @Module({
-  imports: [PrismaModule, MediaModule],
+  imports: [
+    PrismaModule,
+    MediaModule,
+    MulterModule.register(),
+  ],
   controllers: [DocumentsController],
   providers: [DocumentsService],
   exports: [DocumentsService],
